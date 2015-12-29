62f7c53e096a12add07462c285dc7b651510f9e8
Nilesh <bita147@rediffmail.com>
2015-12-29 13:13:28 +0530

    Activity Commit

--- app/src/main/java/com/senta/databinding/MainActivity.java
+++ app/src/main/java/com/senta/databinding/MainActivity.java
@@ -3,15 +3,34 @@ package com.senta.databinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
 import com.senta.databinding.databinding.MainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    MainBinding mainActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainBinding activity = DataBindingUtil.setContentView(this,R.layout.main);
-        activity.etText.setText("Good Evening");
-        activity.tvFirstName.setText("Hi");
+        mainActivity = DataBindingUtil.setContentView(this,R.layout.main);
+        initLayout();
+        mainActivity.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uName = mainActivity.etUsername.getText().toString();
+                String pass = mainActivity.etPassword.getText().toString();
+                if (uName.equalsIgnoreCase("admin") && pass.equalsIgnoreCase("admin")){
+                    Toast.makeText(MainActivity.this,"Login Success",Toast.LENGTH_LONG).show();
+                }else
+                    Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initLayout() {
+        mainActivity.tvLoginCap.setText("Login");
+        mainActivity.etUsername.setText("Admin");
+        mainActivity.etPassword.setText("Admin");
     }
 }
